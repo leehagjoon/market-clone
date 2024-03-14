8e7257f0324bee7261f2e6866fda2141bfcebbbc
hagjoon(이학준) <winner__loser@naver.com>
2024-03-14 18:57:24 +0900

    svelte 활용하여 main,write 페이지 script 추가

--- src/pages/Main.svelte
+++ src/pages/Main.svelte
@@ -1,6 +1,22 @@
 <script>
+  import Footer from "../components/Footer.svelte";
+  import { getDatabase, ref, onValue} from "firebase/database";
+  import { onMount } from "svelte"
+    
     let hour = new Date().getHours();
     let min = new Date().getMinutes();
+
+    $: items = [];
+
+    const  db = getDatabase();
+    const itemsRef = ref(db, "items/");
+
+    onMount(()=> {
+        onValue(itemsRef,(snapshot) =>{
+            const data = snapshot.val();
+            items = Object.values(data);
+        });
+    })
 </script>
 
 <header>
@@ -28,42 +44,20 @@
   </header>
 
   <main>
+    {#each items as item}
+    <div class="item-list">
+        <div class="item-list__img"/>
+        <div class="item-list__info">
+            <div class="item-list__info-title">{item.title}</div>
+            <div class="item-list__info-meta">{item.place}</div>
+            <div class="item-list__info-price">{item.price}</div>
+            <div>{item.description}</div>
+    </div>
+    </div>
+    {/each}
     <a class="write-btn" href="#/write">+ 글쓰기</a>
   </main>
 
-  <footer>
-    <div class="footer-block">
-      <div class="footer-icons">
-        <div class="footer-icons__img">
-          <img src="assets/home.svg" alt="" />
-        </div>
-        <div class="footer-icons__desc">홈</div>
-      </div>
-      <div class="footer-icons">
-        <div class="footer-icons__img">
-          <img src="assets/doc.svg" alt="" />
-        </div>
-        <div class="footer-icons__desc">동네생활</div>
-      </div>
-      <div class="footer-icons">
-        <div class="footer-icons__img">
-          <img src="assets/location.svg" alt="" />
-        </div>
-        <div class="footer-icons__desc">내 근처</div>
-      </div>
-      <div class="footer-icons">
-        <div class="footer-icons__img">
-          <img src="assets/chat.svg" alt="" />
-        </div>
-        <div class="footer-icons__desc">채팅</div>
-      </div>
-      <div class="footer-icons">
-        <div class="footer-icons__img">
-          <img src="assets/user.svg" alt="" />
-        </div>
-        <div class="footer-icons__desc">나의 당근</div>
-      </div>
-    </div>
-  </footer>
-
+  <Footer location = "home"/>
   <div class="media-info-msg">화면 사이즈를 줄여주세요.</div>
+  
