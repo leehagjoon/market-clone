5cc38c4aa280b08cde6af238ad6c29d82fe003cf
hagjoon(이학준) <winner__loser@naver.com>
2024-03-15 17:53:17 +0900

    .

---
+++ firebase.js
@@ -0,0 +1,23 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+    apiKey: import.meta.env.VITE_APP_KEY,
+    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+    databaseURL: import.meta.env.VITE_DB_URL,
+    projectId: import.meta.env.VITE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGE,
+    appId: import.meta.env.VITE_APP_ID
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  
+  
+  // Initialize Realtime Database and get a reference to the service
+  const database = getDatabase(app);
+  const storage = getStorage(app);
+  const auth = getAuth(app);
